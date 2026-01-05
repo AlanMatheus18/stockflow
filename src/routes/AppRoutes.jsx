c669e1637d08402d146d6a45c670d4cd5313e516
AlanMatheus <matheusalan334@gmail.com>
2026-01-05 00:36:17 -0300

    Routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,6 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { AuthProvider } from '../AuthContext';
+
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'; 
+import { AuthProvider, useAuth } from '../AuthContext';
 // Pages
 import Home from '../pages/Home'
 import Login from '../pages/Login'
@@ -17,21 +18,22 @@ const PrivateRoute = ({ children }) => {
 export default function AppRoutes() {
   return (
     <AuthProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/cadastro" element={<FormCadastro />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/movements" element={<Movements />} />
-        <Route path="/reports" element={<Reports />} />
+      <BrowserRouter>
+        <Routes>
+          {/* Rotas Públicas */}
+          <Route path="/" element={<Login />} />
+          <Route path="/cadastro" element={<FormCadastro />} />
 
-        {/* Rotas Protegidas */}
+          {/* Rotas Protegidas (Sempre dentro do PrivateRoute) */}
           <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
           <Route path="/products" element={<PrivateRoute><Products /></PrivateRoute>} />
-          {/* ... outras rotas */}
-      </Routes>
-    </BrowserRouter>
+          <Route path="/movements" element={<PrivateRoute><Movements /></PrivateRoute>} />
+          <Route path="/reports" element={<PrivateRoute><Reports /></PrivateRoute>} />
+          
+          {/* Redirecionamento de segurança: se a rota não existir, vai para o login */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
     </AuthProvider>
-  )
+  );
 }
